4796bd400e0af32f34a1de55c723b9abef5873ae
trong.kd <kienductrong@gmail.com>
2024-09-17 10:05:45 +0700

    Update constant.js

--- src/static/constant.js
+++ src/static/constant.js
@@ -4,7 +4,7 @@ const EmailPattern =
   /^(?!.*[.]{2,})[a-zA-Z0-9.%]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };

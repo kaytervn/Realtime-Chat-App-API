2745bcd2ce7ea95801cfe545ea06a9d2c881e579
trong.kd <kienductrong@gmail.com>
2024-10-07 11:04:59 +0700

    update swagger

--- src/docs/conversationDocs.js
+++ src/docs/conversationDocs.js
@@ -22,12 +22,15 @@
  *             properties:
  *               name:
  *                 type: string
+ *                 description: Name of the conversation (for group chats)
  *               avatarUrl:
  *                 type: string
- *               memberIds:
+ *                 description: URL of the conversation avatar (for group chats)
+ *               conversationMembers:
  *                 type: array
  *                 items:
  *                   type: string
+ *                 description: Array of user IDs to be added to the conversation
  *     responses:
  *       200:
  *         description: Conversation created successfully
@@ -52,10 +55,13 @@
  *             properties:
  *               id:
  *                 type: string
+ *                 description: ID of the conversation to update
  *               name:
  *                 type: string
+ *                 description: New name for the conversation
  *               avatarUrl:
  *                 type: string
+ *                 description: New avatar URL for the conversation
  *     responses:
  *       200:
  *         description: Conversation updated successfully
@@ -77,6 +83,7 @@
  *         required: true
  *         schema:
  *           type: string
+ *         description: ID of the conversation to retrieve
  *     responses:
  *       200:
  *         description: Conversation retrieved successfully
@@ -98,6 +105,7 @@
  *         required: true
  *         schema:
  *           type: string
+ *         description: ID of the conversation to delete
  *     responses:
  *       200:
  *         description: Conversation deleted successfully
@@ -113,6 +121,32 @@
  *     tags: [Conversation]
  *     security:
  *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         description: Filter conversations by name
+ *       - in: query
+ *         name: kind
+ *         schema:
+ *           type: number
+ *         description: Filter conversations by kind (1 for group, 2 for direct)
+ *       - in: query
+ *         name: isPaged
+ *         schema:
+ *           type: string
+ *         description: Set to "0" for unpaged results
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: number
+ *         description: Page number for pagination
+ *       - in: query
+ *         name: size
+ *         schema:
+ *           type: number
+ *         description: Number of items per page
  *     responses:
  *       200:
  *         description: List of conversations retrieved successfully
--- src/docs/conversationMemberDocs.js
+++ src/docs/conversationMemberDocs.js
@@ -20,10 +20,12 @@
  *           schema:
  *             type: object
  *             properties:
- *               conversationId:
+ *               conversation:
  *                 type: string
- *               userId:
+ *                 description: ID of the conversation
+ *               user:
  *                 type: string
+ *                 description: ID of the user to be added
  *     responses:
  *       200:
  *         description: Member added to conversation successfully
@@ -34,7 +36,7 @@
 /**
  * @swagger
  * /v1/conversation-member/remove:
- *   delete:
+ *   put:
  *     summary: Remove a member from a conversation
  *     tags: [ConversationMember]
  *     security:
@@ -46,10 +48,12 @@
  *           schema:
  *             type: object
  *             properties:
- *               conversationId:
+ *               conversation:
  *                 type: string
- *               userId:
+ *                 description: ID of the conversation
+ *               user:
  *                 type: string
+ *                 description: ID of the user to be removed
  *     responses:
  *       200:
  *         description: Member removed from conversation successfully
@@ -59,21 +63,76 @@
 
 /**
  * @swagger
- * /v1/conversation-member/list/{conversationId}:
+ * /v1/conversation-member/list:
  *   get:
  *     summary: Get list of members in a conversation
  *     tags: [ConversationMember]
  *     security:
  *       - bearerAuth: []
  *     parameters:
- *       - in: path
- *         name: conversationId
- *         required: true
+ *       - in: query
+ *         name: conversation
  *         schema:
  *           type: string
+ *         description: ID of the conversation
+ *       - in: query
+ *         name: isPaged
+ *         schema:
+ *           type: string
+ *         description: Set to "0" for unpaged results
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: number
+ *         description: Page number for pagination
+ *       - in: query
+ *         name: size
+ *         schema:
+ *           type: number
+ *         description: Number of items per page
  *     responses:
  *       200:
  *         description: List of conversation members retrieved successfully
+ *       400:
+ *         description: Bad request
+ */
+
+/**
+ * @swagger
+ * /v1/conversation-member/grant:
+ *   put:
+ *     summary: Grant permissions to a conversation member
+ *     tags: [ConversationMember]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               conversation:
+ *                 type: string
+ *                 description: ID of the conversation
+ *               user:
+ *                 type: string
+ *                 description: ID of the user to grant permissions
+ *               canMessage:
+ *                 type: number
+ *                 enum: [0, 1]
+ *                 description: Permission to send messages
+ *               canUpdate:
+ *                 type: number
+ *                 enum: [0, 1]
+ *                 description: Permission to update conversation
+ *               canAddMember:
+ *                 type: number
+ *                 enum: [0, 1]
+ *                 description: Permission to add members
+ *     responses:
+ *       200:
+ *         description: Permissions granted successfully
  *       400:
  *         description: Bad request
  */

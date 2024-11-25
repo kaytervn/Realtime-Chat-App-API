bc8d31c738980e39968fddf4966ddc3d6c59682b
trong.kd <kienductrong@gmail.com>
2024-11-25 15:09:15 +0700

    Update initDbConfig.js

--- src/configurations/initDbConfig.js
+++ src/configurations/initDbConfig.js
@@ -12,19 +12,24 @@ const initDb = async () => {
     const permissionCount = await Permission.countDocuments();
     if (permissionCount === 0) {
       await Permission.insertMany(permissionData);
+      console.log("Permissions inserted successfully.");
     }
     const roleCount = await Role.countDocuments();
     if (roleCount === 0) {
       await Role.insertMany(roleData);
+      console.log("Roles inserted successfully.");
     }
     const settingCount = await Setting.countDocuments();
     if (settingCount === 0) {
       await Setting.insertMany(settingData);
+      console.log("Settings inserted successfully.");
     }
     const userCount = await User.countDocuments();
     if (userCount === 0) {
       await User.insertMany(userData);
+      console.log("Users inserted successfully.");
     }
+    console.log("Database initialized successfully.");
   } catch (error) {
     console.error("Error initializing database:", error);
   }
